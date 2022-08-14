ed7978e17683a7f281d784721916cba536103c89
b4balller <55590243+b4balller@users.noreply.github.com>
2022-08-14 07:39:02 +0200

    New test version 2

--- hello-openshift-for-developers.go
+++ hello-openshift-for-developers.go
@@ -9,7 +9,7 @@ import (
 func helloHandler(w http.ResponseWriter, r *http.Request) {
 	response := os.Getenv("RESPONSE")
 	if len(response) == 0 {
-		response = "Hello OpenShift for Developers and Learners!"
+		response = "Hello OpenShift for Developers and Learners version 2!"
 	}
 
 	fmt.Fprintln(w, response)
